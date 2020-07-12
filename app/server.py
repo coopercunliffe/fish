09c05a451a6ae00363197cf1ac25b09b940d5b7b
Cooper Cunliffe <cooper@Coopers-MacBook-Pro.local>
2020-07-12 17:23:37 -0400

    update template

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/63b31q8d5gdftuf/export.pkl?dl=0'
+export_file_url = 'https://www.dropbox.com/s/63b31q8d5gdftuf/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
 classes = ['trout', 'salmon']
